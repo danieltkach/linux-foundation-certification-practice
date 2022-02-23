7d5e954511996c851857a9642e7723c2dd02866e
Daniel Tkach <danieltkach@gmail.com>
2022-02-23 18:25:13 -0300

    express routes and models

--- my-express-service/routes/bicycle.js
+++ my-express-service/routes/bicycle.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var model = require('../model');
+let express = require('express');
+let router = express.Router();
+let model = require('../model');
 
 router.get('/:id', function(req, res, next) {
   model.bicycle.read(req.params.id, (err, result) => {
@@ -11,7 +11,53 @@ router.get('/:id', function(req, res, next) {
       res.send(result);
     }
   });
+});
 
+router.post('/', function(req, res, next) {
+  let id = model.bicycle.uid();
+  model.bicycle.create(id, req.body.data, (err) => {
+    if (err) next(err);
+    else res.status(201).send({ id });
+  });
+});
+
+router.post('/:id/update', function(req, res, next) {
+  model.bicycle.update(req.params.id, req.body.data, (err) => {
+    if (err) {
+      if (err.message === 'not found') next();
+      else next(err);
+    } else {
+      res.status(204).send();
+    }
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  model.bicycle.create(req.params.id, req.body.data, (err) => {
+    if (err) {
+      if (err.message === 'resource exists') {
+        model.bicycle.update(req.params.id, req.body.data, (err) => {
+          if (err) next(err);
+          else res.status(204).send();
+        });
+      } else {
+        next(err);
+      }
+    } else {
+      res.status(201).send({});
+    }
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  model.bicycle.del(req.params.id, (err) => {
+    if (err) {
+      if (err.message === 'not found') next();
+      else next(err);
+    } else {
+      res.status(204).send();
+    }
+  });
 });
 
 module.exports = router;
