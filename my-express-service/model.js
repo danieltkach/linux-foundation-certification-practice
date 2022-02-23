7c0cec49a0e07a3841f1c0d483ba5da023596ac4
Daniel Tkach <danieltkach@gmail.com>
2022-02-23 18:25:24 -0300

    express routes and models

--- my-express-service/model.js
+++ my-express-service/model.js
@@ -1,25 +1,64 @@
 'use strict'
 
 module.exports = {
-  bicycle: bicycleModel()
+	bicycle: bicycleModel()
 }
 
-function bicycleModel () {
-  const db = {
-    1: { brand: 'Veloretti', color: 'green' },
-    2: { brand: 'Batavus', color: 'yellow' }
-  }
+function bicycleModel() {
+	const db = {
+		1: { brand: 'Veloretti', color: 'green' },
+		2: { brand: 'Batavus', color: 'yellow' }
+	}
 
-  return {
-    read
-  }
+	return {
+		create, read, update, del, uid
+	}
 
-  function read (id, cb) {
-    if (!(db.hasOwnProperty(id))) {
-      const err = Error('not found')
-      setImmediate(() => cb(err))
-      return
-    }
-    setImmediate(() => cb(null, db[id]))
-  }
+	function uid() {
+		return Object.keys(db)
+			.sort((a, b) => a - b)
+			.map(Number)
+			.filter((n) => !isNaN(n))
+			.pop() + 1 + ''
+	}
+
+	function create(id, data, cb) {
+		if (db.hasOwnProperty(id)) {
+			const err = Error('resource exists')
+			setImmediate(() => cb(err))
+			return
+		}
+		db[id] = data
+		setImmediate(() => cb(null, id))
+	}
+
+	function read(id, cb) {
+		if (!(db.hasOwnProperty(id))) {
+			const err = Error('not found')
+			setImmediate(() => cb(err))
+			return
+		}
+		setImmediate(() => cb(null, db[id]))
+	}
+
+	function update(id, data, cb) {
+		if (!(db.hasOwnProperty(id))) {
+			const err = Error('not found')
+			setImmediate(() => cb(err))
+			return
+		}
+		db[id] = data
+		setImmediate(() => cb())
+	}
+
+	function del(id, cb) {
+		if (!(db.hasOwnProperty(id))) {
+			const err = Error('not found')
+			setImmediate(() => cb(err))
+			return
+		}
+		delete db[id]
+		setImmediate(() => cb())
+	}
+
 }
