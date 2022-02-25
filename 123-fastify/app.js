234b4aedfb2c97769d75ec08941816f3cfa6248b
Daniel Tkach <danieltkach@gmail.com>
2022-02-25 14:39:00 -0300

    static html

--- 123-fastify/app.js
+++ 123-fastify/app.js
@@ -15,6 +15,6 @@ export default async function(app, opts) {
 	})
 	
 	app.register(import('fastify-autoload'), {
-		dir: join(import .meta.url, 'routes')
+		dir: join(import.meta.url, 'routes')
 	});
 }
