b9a59eee8320fff320be35a141087eeae722cbeb
Daniel Tkach <danieltkach@gmail.com>
2022-03-05 14:06:24 -0300

    inheritance

---
+++ prototypal-inheritance.js
@@ -0,0 +1,17 @@
+class Person {
+	constructor(name){
+		this.name = name;
+	}
+	sayName() {
+		console.log(this.name)
+	}
+}
+
+class Student extends Person {
+	constructor(name) {
+		super(name);
+	}
+}
+
+const codeWitcher = new Student('Daniel T.');
+codeWitcher.sayName();
